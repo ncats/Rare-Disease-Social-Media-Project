b6b2a143d5a5437405ae161d7197e556ec0b0185
Devon Leadman <shanmardev@hotmail.com>
2022-06-15 13:29:13 -0400

    delete all

--- mapper/bin/__init__.py
+++
@@ -1,9 +0,0 @@
-from AbstractMap import AbstractMap
-from RedditMap import RedditMap
-from FalsePositives import FalsePositives
-
-if __name__ == '__main__':
-    map = AbstractMap()
-    map._match('10000_grantAbstracts.xlsx','neo4j_rare_disease_list.json')
-    
-    
