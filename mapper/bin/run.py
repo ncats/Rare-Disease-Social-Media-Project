20f309f7138cef4dacec734f123d9b560cfa6f83
Bradley Allen Karas <bkaras1@asu.edu>
2022-08-31 10:27:18 -0400

    Removed old file structure.

--- mapper/bin/run.py
+++
@@ -1,8 +0,0 @@
-from AbstractMap import AbstractMap
-#from FalsePositives import FalsePositives
-
-if __name__ == '__main__':
-    map = AbstractMap()
-    map._match('filtered_abs.txt','neo4j_rare_disease_list.json', IDcol='Application_ID', TEXTcols=['Abstract'])
-    # 'Condition','OfficialTitle','BriefTitle','BriefSummary','Keyword','ArmGroupDescription','DetailedDescription'
-    # Order of list dictates weight
--- topic_modeling/reddit_scripts/__init__.py
+++
@@ -1,7 +0,0 @@
-from .get_comment_data import GetRedditComments
-from .get_post_data import GetPosts
-
-__all__ = [
-           'GetRedditComments',
-           'GetPosts',
-           ]
