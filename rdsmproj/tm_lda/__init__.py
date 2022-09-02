e73dbd27379ac72d393437ce3aa4aeae560648aa
Bradley Allen Karas <bkaras1@asu.edu>
2022-09-01 23:05:22 -0400

    Updating for TestPyPI

--- rdsmproj/tm_lda/__init__.py
+++ rdsmproj/tm_lda/__init__.py
@@ -5,3 +5,4 @@ from rdsmproj.tm_lda import topic_tools
 from rdsmproj.tm_lda import main_legacy
 
 __author__ = 'Bradley Karas'
+__author_email__ = 'bradley.karas@gmail.com'
--- rdsmproj/tm_t2v/__init__.py
+++ rdsmproj/tm_t2v/__init__.py
@@ -2,3 +2,4 @@ from rdsmproj.tm_t2v.top2vec_model import Top2VecModel
 from rdsmproj.tm_t2v.top2vec_topic_tools import AnalyzeTopics
 
 __author__ = 'Bradley Karas'
+__author_email__ = 'bradley.karas@gmail.com'
